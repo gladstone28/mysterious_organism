f005cd91178d45b5827a33854c8974380ec9a76a
Gladstone Rose <gladstonerose@hotmail.com>
2023-04-21 15:13:25 -0500

    completed step 5 in the Project

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ const pAequorFactory = (num, dnaArr) => {
 
 */
 
+/*
 
 const pAequorFactory = (num, dnaArr) => {
   return {
@@ -105,6 +106,68 @@ const mypAequor = pAequorFactory(1, mockUpStrand());
 console.log(mypAequor.dna); // original dna
 mypAequor.mutate();
 console.log(mypAequor.dna); // mutated dna
+
+*/
+
+const pAequorFactory = (num, dnaArr) => {
+  return {
+    specimenNum: num,
+    dna: dnaArr,
+    mutate() {
+      const randIndex = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+      while (newBase === this.dna[randIndex]) {
+        newBase = returnRandBase();
+      }
+      this.dna[randIndex] = newBase;
+      return this.dna;
+    },
+    compareDNA(pAequor) {
+      const identicalBases = this.dna.reduce((acc, curr, idx, arr) => {
+        if (pAequor.dna[idx] === curr) {
+          return acc + 1;
+        } else {
+          return acc;
+        }
+      }, 0);
+      const percentIdentical = ((identicalBases / this.dna.length) * 100).toFixed(2);
+      console.log(`${this.specimenNum} and ${pAequor.specimenNum} have ${percentIdentical}% DNA in common.`);
+    },
+    willLikelySurvive() {
+      const cOrGCount = this.dna.filter(base => base === 'C' || base === 'G').length;
+      const percentCG = (cOrGCount / this.dna.length) * 100;
+      return percentCG >= 60;
+    },
+    complementStrand() {
+      const basePairs = {
+        A: 'T',
+        T: 'A',
+        C: 'G',
+        G: 'C'
+      };
+      return this.dna.map(base => basePairs[base]);
+    }
+  }
+};
+
+
+const pAequor1 = pAequorFactory(1, mockUpStrand());
+const pAequor2 = pAequorFactory(2, mockUpStrand());
+pAequor1.compareDNA(pAequor2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
